bc8800e241b356ee31540574827f98ed4f82370f
Ava Chaney <hello@ava.dev>
2023-06-23 00:33:38 -0700

    nuke it all

--- components/llrt/src/runtime/engine/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod bindings;
-pub mod module;
-pub mod types;
-
-mod engine;
-mod error;
-
-pub use engine::*;
-pub use error::*;
--- components/llrt/src/runtime/mod.rs
+++ components/llrt/src/runtime/mod.rs
@@ -1,30 +1,26 @@
 pub mod callable;
-pub mod context;
-pub mod engine;
 
-use crate::runtime::context::ContextOptions;
 use log::info;
 use std::{borrow::Cow, path::PathBuf};
 
-const CORE_RUNTIME_KEY: &str = "HLRT";
 pub async fn init_hlrt_context(runtime_dir: &PathBuf) -> anyhow::Result<()> {
     info!("initializing high-level runtime context");
-    let hlrt_ctx = context::JsContext::create(ContextOptions {
-        id: Cow::from(CORE_RUNTIME_KEY),
-        module_base_path: Some(runtime_dir.clone()),
-    })
-    .await?;
+    // let hlrt_ctx = context::JsContext::create(ContextOptions {
+    //     id: Cow::from(CORE_RUNTIME_KEY),
+    //     module_base_path: Some(runtime_dir.clone()),
+    // })
+    // .await?;
 
-    info!("loading core runtime");
-    hlrt_ctx
-        .execute_code(
-            "Grebuloff.LLRT.print('hello from Grebuloff.LLRT.print()!');
-                        Grebuloff.LLRT.create_context('context_from_hlrt');
-                        Grebuloff.LLRT.print('we did the thing! testing import...');
-                        import('test import');
-                        Grebuloff.LLRT.print('past the import');",
-        )
-        .await?;
+    // info!("loading core runtime");
+    // hlrt_ctx
+    //     .execute_code(
+    //         "Grebuloff.LLRT.print('hello from Grebuloff.LLRT.print()!');
+    //                     Grebuloff.LLRT.create_context('context_from_hlrt');
+    //                     Grebuloff.LLRT.print('we did the thing! testing import...');
+    //                     import('test import');
+    //                     Grebuloff.LLRT.print('past the import');",
+    //     )
+    //     .await?;
 
     Ok(())
 }
