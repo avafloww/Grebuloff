f2e603e0353141da5549ab7e8c655df7ccb267f3
Ava Chaney <hello@ava.dev>
2023-07-07 15:34:08 -0700

    placebo? you tell me

--- components/llrt/src/hooking/shaders/ps.hlsl
+++ components/llrt/src/hooking/shaders/ps.hlsl
@@ -2,5 +2,9 @@
 
 float4 main(Interpolators In) : SV_Target0
 {
-    return Texture.Sample(PointSampler, In.TexCoord);
+    float4 col = Texture.Sample(PointSampler, In.TexCoord);
+
+    // Electron for Windows uses RGBA, and even though we can specify the texture as BGRA, it still
+    // seems to perform faster when we swizzle the colour here instead of changing the D3D11_TEXTURE2D_DESC.
+    return float4(col.b, col.g, col.r, col.a);
 }
